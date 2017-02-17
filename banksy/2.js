9118f05e5892f6ae2d4118c7a3eb6e7a9bc3deba
Evan Cole <master@Evans-MacBook-Pro.local>
2017-02-17 14:29:08 +0100

    first date

---
+++ banksy/2.js
@@ -0,0 +1,16 @@
+//  with an object instead of a variable
+
+var accObj = {cash: 0};
+
+
+// ---------------------------------- ///
+
+console.log(accObj.cash);
+
+accObj.cash++;
+
+console.log(accObj.cash);
+
+accObj.cash--;
+
+console.log(accObj.cash);
